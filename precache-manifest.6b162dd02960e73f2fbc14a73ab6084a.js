661c8d89d344f3f2069077a7666f4ff74301e71c
CreatorLZ <isaacanyim3@gmail.com>
2022-06-30 20:01:29 +1300

    Updates

---
+++ precache-manifest.6b162dd02960e73f2fbc14a73ab6084a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51ddd18d10ee24e216f",
+    "url": "/NEW-APP/static/css/main.d02f89db.chunk.css"
+  },
+  {
+    "revision": "e51ddd18d10ee24e216f",
+    "url": "/NEW-APP/static/js/main.983cdc12.chunk.js"
+  },
+  {
+    "revision": "cd20ce5bb23816cd9915",
+    "url": "/NEW-APP/static/js/runtime~main.4cee6653.js"
+  },
+  {
+    "revision": "3e46f7259c33f483fe74",
+    "url": "/NEW-APP/static/js/2.c483d079.chunk.js"
+  },
+  {
+    "revision": "152080b6073f97716861",
+    "url": "/NEW-APP/static/js/3.af919cd0.chunk.js"
+  },
+  {
+    "revision": "fc2a973f4c53de7e3ff6dc5b27b8dc24",
+    "url": "/NEW-APP/index.html"
+  }
+];
