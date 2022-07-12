1bff159c2ba17c3f201a657dbea4a3f73c363314
CreatorLZ <isaacanyim3@gmail.com>
2022-07-12 21:52:38 +1300

    hope this works

--- api/index.js
+++ api/index.js
@@ -34,6 +34,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Backend server is running!" + PORT);
 });
